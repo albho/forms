5d088f16eb5dcbb29f72f110bce673cfc1d4a85c
Albert Ho <albertcyi.ho@gmail.com>
2023-01-15 10:45:52 -0800

    ts setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+
+// Components
+import Button, { ButtonVariant } from './components/button.tsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button
+        variant={ButtonVariant.BLUE}
+        onClick={() => console.log('Submitted')}
+      >
+        Submit
+      </Button>
     </div>
   );
 }
